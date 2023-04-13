bb80b5c7eabaf2d3e54637e97d9f5f50383b225f
Will DePue <will@depue.net>
2023-04-12 20:43:41 -0700

    Running away is easy, it's the leaving that's ha-ard. Layer norm done

--- layer_norm.js
+++ layer_norm.js
@@ -13,13 +13,12 @@ const createNormStatsShader = () => `
   @group(0) @binding(0) var<uniform> DimBuffer: Dimensions;
   @group(0) @binding(1) var<storage, read_write> Result: Matrix;
 
-  @compute @workgroup_size(16)
+  @compute @workgroup_size(16, 16)
   fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
     let row: u32 = global_id.x;
     let dimX: u32 = DimBuffer.dimX;
-    let dimY: u32 = DimBuffer.dimY;
 
-    if (row >= dimY) {
+    if (row >= DimBuffer.dimY) {
       return;
     }
 
@@ -34,18 +33,57 @@ const createNormStatsShader = () => `
         variance = variance + (Input.data[row * dimX + i] - mean) * (Input.data[row * dimX + i] - mean);
     }
     variance = variance / f32(dimX);
-    var stdeviation: f32 = sqrt(variance);
+    var stdev: f32 = sqrt(variance + 1e-5);
 
     Result.data[row * 2] = mean;
-    Result.data[row * 2 + 1] = stdeviation;
+    Result.data[row * 2 + 1] = stdev;
   }
   `;
 
-async function layerNorm(rows, cols, input) {
+const createNormShader = () => `
+  struct Matrix {
+      data: array<f32>, // runtime-sized array
+  }
+
+  struct Dimensions {
+    dimY: u32, // row dimension of input matrix
+    dimX: u32, // col dimension of input matrix
+    gamma: f32, // network parameter
+    beta: f32, // network parameter
+  };
+
+  @group(0) @binding(0) var<uniform> DimBuffer: Dimensions;
+  @group(0) @binding(1) var<storage, read_write> Result: Matrix;
+  
+  @group(1) @binding(0) var<storage, read> Input: Matrix;
+  @group(2) @binding(0) var<storage, read> Stats: Matrix;
+
+  @compute @workgroup_size(16, 16)
+  fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
+      let row: u32 = global_id.x;
+      let col: u32 = global_id.y;
+      let dimX: u32 = DimBuffer.dimX;
+      let dimY: u32 = DimBuffer.dimY;
+
+      if (row >= dimY || col >= dimX) {
+        return;
+      }
+
+      let mean = Stats.data[row * 2];
+      let stdev = Stats.data[row * 2 + 1];
+      let output = (Input.data[row * dimX + col] - mean) / stdev;
+      let shift = DimBuffer.gamma * output + DimBuffer.beta;
+      Result.data[row * dimX + col] = shift;
+    } 
+  `;
+
+async function layerNorm(rows, cols, input, gamma = 1, beta = 0) {
   const { device, queue } = await initializeWebGPU();
   const minStorageBufferOffsetAlignment = device.limits.minStorageBufferOffsetAlignment;
   const bufferSizeCalc = (dimA, dimB = 1) => alignedSize(dimA * dimB * Float32Array.BYTES_PER_ELEMENT, minStorageBufferOffsetAlignment);
-  const workgroup_size_X = 16; // Dictated by shader.
+  const workgroup_stats_X = 16; // Dictated by shader.
+  const workgroup_norm_X = 16; // Dictated by shader.
+  const workgroup_norm_Y = 16; // Dictated by shader.
 
   // Generic bind group for input buffer, will be reused.
   const inputBufferBindGroupLayout = createBindGroupLayout(device, ["read-only-storage"]);
@@ -53,8 +91,8 @@ async function layerNorm(rows, cols, input) {
   const statsBindGroupLayout = createBindGroupLayout(device, ["uniform", "storage"]);
   const statsPipeline = createPipeline(device, createNormStatsShader(), [statsBindGroupLayout, inputBufferBindGroupLayout]);
   // NORM pipeline, will be reused.
-  //   const normBindGroupLayout = createBindGroupLayout(device, ["uniform", "storage"]);
-  //   const normPipeline = createPipeline(device, createNormShader(), [normBindGroupLayout, inputBufferBindGroupLayout]);
+  const normBindGroupLayout = createBindGroupLayout(device, ["uniform", "storage"]);
+  const normPipeline = createPipeline(device, createNormShader(), [normBindGroupLayout, inputBufferBindGroupLayout, inputBufferBindGroupLayout]);
 
   console.log("Starting network");
   const inputBuffer = createBuffer(device, bufferSizeCalc(rows, cols), GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST);
@@ -65,22 +103,40 @@ async function layerNorm(rows, cols, input) {
   const statsBindGroup = createBindGroup(device, statsBindGroupLayout, [statsUniformBuffer, statsResultBuffer]);
   queue.writeBuffer(statsUniformBuffer, 0, new Uint32Array([rows, cols]));
 
-  const pipeline = statsPipeline;
+  const normUniformBuffer = createBuffer(device, 16, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST);
+  const normResultBuffer = createBuffer(device, bufferSizeCalc(rows, cols), GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC);
+  const normBindGroup = createBindGroup(device, normBindGroupLayout, [normUniformBuffer, normResultBuffer]);
+  queue.writeBuffer(normUniformBuffer, 0, new Uint32Array([rows, cols]));
+  queue.writeBuffer(normUniformBuffer, 8, new Float32Array([gamma, beta]));
+
+  /*
+
+  This throws an error because rows and cols are both uint32 but gamma and beta are float32.
+
+  */
 
   console.log("Starting passes");
   const commandEncoder = device.createCommandEncoder();
 
-  const passEncoder = commandEncoder.beginComputePass();
-  passEncoder.setPipeline(pipeline);
-  passEncoder.setBindGroup(0, statsBindGroup);
-  passEncoder.setBindGroup(1, createBindGroup(device, inputBufferBindGroupLayout, [inputBuffer]));
-  passEncoder.dispatchWorkgroups(workgroupCalc(rows, workgroup_size_X));
-  passEncoder.end();
+  const passEncoder1 = commandEncoder.beginComputePass();
+  passEncoder1.setPipeline(statsPipeline);
+  passEncoder1.setBindGroup(0, statsBindGroup);
+  passEncoder1.setBindGroup(1, createBindGroup(device, inputBufferBindGroupLayout, [inputBuffer]));
+  passEncoder1.dispatchWorkgroups(workgroupCalc(rows, workgroup_stats_X));
+  passEncoder1.end();
 
-  const outputBufferSize = bufferSizeCalc(rows, 2);
+  const passEncoder2 = commandEncoder.beginComputePass();
+  passEncoder2.setPipeline(normPipeline);
+  passEncoder2.setBindGroup(0, normBindGroup);
+  passEncoder2.setBindGroup(1, createBindGroup(device, inputBufferBindGroupLayout, [inputBuffer]));
+  passEncoder2.setBindGroup(2, createBindGroup(device, inputBufferBindGroupLayout, [statsResultBuffer]));
+  passEncoder2.dispatchWorkgroups(workgroupCalc(rows, workgroup_norm_Y), workgroupCalc(cols, workgroup_norm_X));
+  passEncoder2.end();
+
+  const outputBufferSize = bufferSizeCalc(rows, cols);
   const readBuffer = createBuffer(device, outputBufferSize, GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ);
   const copyCommandEncoder = device.createCommandEncoder();
-  copyCommandEncoder.copyBufferToBuffer(statsResultBuffer, 0, readBuffer, 0, outputBufferSize);
+  copyCommandEncoder.copyBufferToBuffer(normResultBuffer, 0, readBuffer, 0, outputBufferSize);
 
   queue.submit([commandEncoder.finish(), copyCommandEncoder.finish()]);
 
@@ -99,7 +155,7 @@ async function layerNorm(rows, cols, input) {
   printMatrix(row, col, input);
   const result = await layerNorm(row, col, input);
 
-  printMatrix(row, 2, new Float32Array(result));
+  printMatrix(row, col, new Float32Array(result));
 })();
 
 function printMatrix(rows, cols, array) {
@@ -109,7 +165,14 @@ function printMatrix(rows, cols, array) {
   }
   console.log(matrix);
   for (const row of matrix) {
-    console.log(row);
+    console.log(row.reduce((a, b) => a + b) / row.length);
+    console.log(getStandardDeviation(row));
   }
   return matrix;
+}
+
+function getStandardDeviation(array) {
+  const n = array.length;
+  const mean = array.reduce((a, b) => a + b) / n;
+  return Math.sqrt(array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
 }
