74ce83812930c689174867178d8858e18015378f
Will DePue <will@depue.net>
2023-04-21 17:49:55 -0700

    De-embeddings on GPU done!

--- main.js
+++ main.js
@@ -141,114 +141,6 @@ async function runGPT(idx) {
 
   const layerNormOutputBuffer = inlineLayerNorm(device, queue, commandEncoder, seq_length, n_embd, layerOutputBuffer, normGammaBuffer, normBetaBuffer);
 
-  const outputBuffer = createOutputBuffer(device, commandEncoder, layerNormOutputBuffer, seq_length, n_embd);
-
-  queue.submit([commandEncoder.finish()]);
-
-  await outputBuffer.mapAsync(GPUMapMode.READ);
-  const output = outputBuffer.getMappedRange();
-  const deEmbed = deEmbedCPU(output, embeddingWeights, seq_length, n_embd, vocab_size);
-
-  return new Float32Array(deEmbed);
-}
-
-async function runGPTNew(idx) {
-  if (!modelParams) {
-    console.log("Model not loaded yet");
-    return;
-  }
-
-  const { device, queue, params, posEmbdBuffer, layer_buffers, normGammaBuffer, normBetaBuffer, embeddingWeights } = modelParams;
-  const { attentionDotProductScale, n_embd, n_head, n_layer, vocab_size, hidden_size } = params;
-  const seq_length = idx.length;
-
-  const embeddings = idx.map((token) => embeddingWeights.slice(token * n_embd, (token + 1) * n_embd));
-  const flattened = flattenEmbeddings(embeddings, n_embd, seq_length);
-  const embdOutputBuffer = createBuffer(device, bufferSizeCalc(seq_length, n_embd), GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST);
-  queue.writeBuffer(embdOutputBuffer, 0, flattened);
-
-  const commandEncoder = device.createCommandEncoder();
-
-  // Crop the position embeddings to the correct size.
-  const posEmbdOutputBuffer = createBuffer(
-    device,
-    bufferSizeCalc(seq_length, n_embd),
-    GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
-  );
-  commandEncoder.copyBufferToBuffer(
-    posEmbdBuffer, // Source buffer (original position embeddings)
-    0, // Source offset (starting from the beginning of the buffer)
-    posEmbdOutputBuffer, // Destination buffer (cropped buffer)
-    0, // Destination offset (starting from the beginning of the cropped buffer)
-    bufferSizeCalc(seq_length, n_embd) // Number of bytes to copy
-  );
-  // Residual connection is just elementwise addition, can be used for combining embedding and position embedding.
-  const embeddedInputBuffer = inlineResidual(device, queue, commandEncoder, seq_length, n_embd, embdOutputBuffer, posEmbdOutputBuffer);
-  let layerOutputBuffer = embeddedInputBuffer;
-
-  for (let i = 0; i < n_layer; i++) {
-    const buffers = layer_buffers[i];
-
-    const layerNormAttentionOutputBuffer = inlineLayerNorm(
-      device,
-      queue,
-      commandEncoder,
-      seq_length,
-      n_embd,
-      layerOutputBuffer,
-      buffers.normAttentionGammaBuffer,
-      buffers.normAttentionBetaBuffer
-    );
-
-    const attentionOutputBuffer = inlineAttention(
-      device,
-      queue,
-      commandEncoder,
-      seq_length,
-      n_embd,
-      attentionDotProductScale,
-      layerNormAttentionOutputBuffer,
-      n_head,
-      buffers.qkvWeightsBuffer,
-      buffers.qkvBiasBuffer,
-      buffers.linearWeightsBuffer,
-      buffers.linearBiasBuffer
-    );
-
-    const residualAttentionOutputBuffer = inlineResidual(device, queue, commandEncoder, seq_length, n_embd, attentionOutputBuffer, layerOutputBuffer);
-
-    const layerNormLinearOutputBuffer = inlineLayerNorm(
-      device,
-      queue,
-      commandEncoder,
-      seq_length,
-      n_embd,
-      residualAttentionOutputBuffer,
-      buffers.normLinearGammaBuffer,
-      buffers.normLinearBetaBuffer
-    );
-
-    const linearOutputBuffer = inlineFFN(
-      device,
-      queue,
-      commandEncoder,
-      seq_length,
-      n_embd,
-      hidden_size,
-      layerNormLinearOutputBuffer,
-      buffers.firstLayerWeightsBuffer,
-      buffers.firstLayerBiasBuffer,
-      buffers.secondLayerWeightsBuffer,
-      buffers.secondLayerBiasBuffer
-    );
-
-    const residualLinearOutputBuffer = inlineResidual(device, queue, commandEncoder, seq_length, n_embd, linearOutputBuffer, residualAttentionOutputBuffer);
-
-    layerOutputBuffer = residualLinearOutputBuffer;
-  }
-
-  const layerNormOutputBuffer = inlineLayerNorm(device, queue, commandEncoder, seq_length, n_embd, layerOutputBuffer, normGammaBuffer, normBetaBuffer);
-
   /* 
     Compute Pass Splitting: Possible Approaches.
 
@@ -288,10 +180,15 @@ async function runGPTNew(idx) {
 
   // Assumes that vocab_size has a decent least prime factor.
   const maxStorageBufferSize = device.limits.maxStorageBufferBindingSize;
-  const totalElements = n_embd * vocab_size;
+  const totalElements = bufferSizeCalc(vocab_size, n_embd);
   var numInstances = Math.ceil(totalElements / maxStorageBufferSize);
   if (numInstances > 1) numInstances = leastPrimeFactor(vocab_size, numInstances);
   var vocabChunkSize = vocab_size / numInstances;
+
+  console.log("maxStorageBufferSize: " + maxStorageBufferSize);
+  console.log("totalElements: " + totalElements);
+  console.log("numInstances: " + numInstances);
+  console.log("vocabChunkSize: " + vocabChunkSize);
 
   for (let i = 0; i < numInstances; i++) {
     const deEmbedChunkInputBuffer = createBuffer(
