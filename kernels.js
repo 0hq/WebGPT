2013cd7aa3b89715ef7ba5fbc96befdf96e32c4e
Will DePue <will@depue.net>
2023-04-28 01:36:17 -0700

    Kernel speed-ups

--- kernels.js
+++ kernels.js
@@ -198,7 +198,6 @@ const divideShader = `
     }
 `;
 
-// Lots of code from webgpu-blas.
 const fastMatMulShader = `
   struct CMeta {
     M: u32,
@@ -250,36 +249,59 @@ const fastMatMulShader = `
     var sum13: vec4<f32> = vec4<f32>();
 
     for(var k: u32 = 0u; k < KD4; k = k + 1u) {
-      var arow0: vec4<f32> = vec4<f32>();
-      var arow1: vec4<f32> = vec4<f32>();
-      var arow2: vec4<f32> = vec4<f32>();
-      var arow3: vec4<f32> = vec4<f32>();
-      
-      arow0 = array_a[(y * 4u + 0u) * KD4 + k];
-      arow1 = array_a[(y * 4u + 1u) * KD4 + k];
-      arow2 = array_a[(y * 4u + 2u) * KD4 + k];
-      arow3 = array_a[(y * 4u + 3u) * KD4 + k];
+      var arow0: vec4<f32> = array_a[(y * 4u + 0u) * KD4 + k];
+      var arow1: vec4<f32> = array_a[(y * 4u + 1u) * KD4 + k];
+      var arow2: vec4<f32> = array_a[(y * 4u + 2u) * KD4 + k];
+      var arow3: vec4<f32> = array_a[(y * 4u + 3u) * KD4 + k];
+      var brow: vec4<f32>;
 
-      for (var ki: u32 = 0u; ki < 4u; ki = ki + 1u) {
-        var brow: vec4<f32>;
-    
-        let arow0_val = selectValueFromVec4(arow0, ki);
-        let arow1_val = selectValueFromVec4(arow1, ki);
-        let arow2_val = selectValueFromVec4(arow2, ki);
-        let arow3_val = selectValueFromVec4(arow3, ki);
-    
-        brow = array_b[(k * 4u + ki) * ND4 + x * 2u + 0u];
-        sum00 = vec4<f32>(arow0_val) * brow + sum00;
-        sum01 = vec4<f32>(arow1_val) * brow + sum01;
-        sum02 = vec4<f32>(arow2_val) * brow + sum02;
-        sum03 = vec4<f32>(arow3_val) * brow + sum03;
-    
-        brow = array_b[(k * 4u + ki) * ND4 + x * 2u + 1u];
-        sum10 = vec4<f32>(arow0_val) * brow + sum10;
-        sum11 = vec4<f32>(arow1_val) * brow + sum11;
-        sum12 = vec4<f32>(arow2_val) * brow + sum12;
-        sum13 = vec4<f32>(arow3_val) * brow + sum13;
-      }
+      brow = array_b[(k * 4u + 0u) * ND4 + x * 2u + 0u];
+      sum00 = vec4<f32>(arow0.x) * brow + sum00;
+      sum01 = vec4<f32>(arow1.x) * brow + sum01;
+      sum02 = vec4<f32>(arow2.x) * brow + sum02;
+      sum03 = vec4<f32>(arow3.x) * brow + sum03;
+
+      brow = array_b[(k * 4u + 0u) * ND4 + x * 2u + 1u];
+      sum10 = vec4<f32>(arow0.x) * brow + sum10;
+      sum11 = vec4<f32>(arow1.x) * brow + sum11;
+      sum12 = vec4<f32>(arow2.x) * brow + sum12;
+      sum13 = vec4<f32>(arow3.x) * brow + sum13;
+
+      brow = array_b[(k * 4u + 1u) * ND4 + x * 2u + 0u];
+      sum00 = vec4<f32>(arow0.y) * brow + sum00;
+      sum01 = vec4<f32>(arow1.y) * brow + sum01;
+      sum02 = vec4<f32>(arow2.y) * brow + sum02;
+      sum03 = vec4<f32>(arow3.y) * brow + sum03;
+
+      brow = array_b[(k * 4u + 1u) * ND4 + x * 2u + 1u];
+      sum10 = vec4<f32>(arow0.y) * brow + sum10;
+      sum11 = vec4<f32>(arow1.y) * brow + sum11;
+      sum12 = vec4<f32>(arow2.y) * brow + sum12;
+      sum13 = vec4<f32>(arow3.y) * brow + sum13;
+
+      brow = array_b[(k * 4u + 2u) * ND4 + x * 2u + 0u];
+      sum00 = vec4<f32>(arow0.z) * brow + sum00;
+      sum01 = vec4<f32>(arow1.z) * brow + sum01;
+      sum02 = vec4<f32>(arow2.z) * brow + sum02;
+      sum03 = vec4<f32>(arow3.z) * brow + sum03;
+
+      brow = array_b[(k * 4u + 2u) * ND4 + x * 2u + 1u];
+      sum10 = vec4<f32>(arow0.z) * brow + sum10;
+      sum11 = vec4<f32>(arow1.z) * brow + sum11;
+      sum12 = vec4<f32>(arow2.z) * brow + sum12;
+      sum13 = vec4<f32>(arow3.z) * brow + sum13;
+
+      brow = array_b[(k * 4u + 3u) * ND4 + x * 2u + 0u];
+      sum00 = vec4<f32>(arow0.w) * brow + sum00;
+      sum01 = vec4<f32>(arow1.w) * brow + sum01;
+      sum02 = vec4<f32>(arow2.w) * brow + sum02;
+      sum03 = vec4<f32>(arow3.w) * brow + sum03;
+
+      brow = array_b[(k * 4u + 3u) * ND4 + x * 2u + 1u];
+      sum10 = vec4<f32>(arow0.w) * brow + sum10;
+      sum11 = vec4<f32>(arow1.w) * brow + sum11;
+      sum12 = vec4<f32>(arow2.w) * brow + sum12;
+      sum13 = vec4<f32>(arow3.w) * brow + sum13;
     }
 
     if (y * 4u + 0u < M) {
