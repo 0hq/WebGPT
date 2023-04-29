dd59055be9cedcd2df324c639fafe83f231db11c
Will DePue <will@depue.net>
2023-04-28 17:47:06 -0700

    Painful improvements

--- other/scratchpad.js
+++ other/scratchpad.js
@@ -29,26 +29,25 @@ const slowRowAddShader = `
   struct BMeta {
     M: u32,
     N: u32,
-    ND4: u32,
   }
 
-  @group(1) @binding(0) var<storage,read> array_matrix: array<vec4<f32>>;
-  @group(1) @binding(1) var<storage,read> array_bias: array<vec4<f32>>;
+  @group(1) @binding(0) var<storage,read> array_matrix: array<f32>;
+  @group(1) @binding(1) var<storage,read> array_bias: array<f32>;
   @group(0) @binding(0) var<uniform> bmeta: BMeta;
-  @group(0) @binding(1) var<storage,read_write> array_output: array<vec4<f32>>;
+  @group(0) @binding(1) var<storage,read_write> array_output: array<f32>;
 
   @compute @workgroup_size(8,8)
   fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-    var row: u32 = global_id.x;
-    var col: u32 = global_id.y;
-    var ND4: u32 = bmeta.ND4;
+    var col: u32 = global_id.x;
+    var row: u32 = global_id.y;
+    var N: u32 = bmeta.N;
     var M: u32 = bmeta.M;
     
-    if (row >= M || col >= ND4) {
+    if (row >= M || col >= N) {
       return;
     }
 
-    array_output[row * ND4 + col] = array_matrix[row * ND4 + col] + array_bias[col];
+    array_output[row * N + col] = array_matrix[row * N + col] + array_bias[col];
   }
 `;
 
@@ -74,12 +73,6 @@ class TestGPT {
     this.unloadDeletionStack = [];
   }
 
-  unload() {
-    for (let i = 0; i < this.unloadDeletionStack.length; i++) {
-      this.unloadDeletionStack[i].destroy();
-    }
-  }
-
   async initialize() {
     if (this.initialized) return console.error("Model already initialized");
     if (!navigator.gpu) throw new Error("WebGPU is not supported");
@@ -88,66 +81,54 @@ class TestGPT {
     this.device = await adapter.requestDevice();
 
     this.initBindGroups();
-    this.initPipelines();
+    await this.initPipelines();
 
     this.initialized = true;
   }
 
-  async testFastRowAdd(iters = 1000, runs = 10, retries = 5) {
-    const dimM = 1024;
-    const dimN = 1024;
+  async testFastRowAdd(iters = 5, runs = 10, retries = 3) {
+    const dimM = 1000;
+    const dimN = 25128;
     const matrixA = new Float32Array(dimM * dimN);
     const matrixB = new Float32Array(dimN);
     for (let i = 0; i < dimM * dimN; i++) matrixA[i] = i + 1;
     for (let i = 0; i < dimN; i++) matrixB[i] = i + 1;
-
-    const matrixABuffer = this.initBuffer(["storage", "copy_to"], dimM, dimN);
-    this.device.queue.writeBuffer(matrixABuffer, 0, matrixA);
-    const matrixBBuffer = this.initBuffer(["storage", "copy_to"], dimN);
+    const matrixABuffer = this.initBuffer(["storage", "copy_to"], dimM, dimN, 1, true);
+    const matrixBBuffer = this.initBuffer(["storage", "copy_to"], dimN, 1, true);
     this.device.queue.writeBuffer(matrixBBuffer, 0, matrixB);
+    this.device.queue.writeBuffer(matrixABuffer, 0, matrixA);
 
+    let times = [];
     for (let i = 0; i < retries; i++) {
       let runTime = 0;
       for (let i = 0; i < runs; i++) {
         const startTime = performance.now();
-        let destroyBuf = [];
         const commandEncoder = this.device.createCommandEncoder();
         for (let i = 0; i < iters; i++) {
-          const fastRowAddResult = this.inlineFastRowAdd(commandEncoder, matrixABuffer, matrixBBuffer, dimM, dimN);
-          destroyBuf.push(fastRowAddResult);
+          this.inlineSlowRowAdd(commandEncoder, matrixABuffer, matrixBBuffer, dimM, dimN);
         }
         this.device.queue.submit([commandEncoder.finish()]);
-        for (let i = 0; i < destroyBuf.length; i++) destroyBuf[i].destroy();
+        this.destroyBuffers();
         const endTime = performance.now();
         runTime += endTime - startTime;
       }
-      console.log(`${runs} runs of ${dimM}x${dimN} took ${runTime}ms`);
-      console.log(`average time: ${runTime / runs}ms`);
+
+      console.log(`${runs * iters} runs of ${dimM}x${dimN} took ${runTime}ms`);
+      console.log(`average time: ${runTime / (runs * iters)}ms`);
+      times.push(runTime);
     }
+    this.unloadBuffers();
 
-    // const commandEncoder = this.device.createCommandEncoder();
-    // const fastRowAddResult = this.inlineSlowRowAdd(commandEncoder, matrixABuffer, matrixBBuffer, dimM, dimN);
-    // const outputBuffer = this.initOutputBuffer(commandEncoder, fastRowAddResult, dimM, dimN);
-    // this.device.queue.submit([commandEncoder.finish()]);
-    // fastRowAddResult.destroy();
+    // Average of times
+    let sum = 0;
+    for (let i = 0; i < times.length; i++) {
+      sum += times[i];
+    }
+    console.log("Average:", sum / times.length);
 
-    // matrixABuffer.destroy();
-    // matrixBBuffer.destroy();
-
-    // await outputBuffer.mapAsync(GPUMapMode.READ);
-    // const output = outputBuffer.getMappedRange();
-    // const outputArray = new Float32Array(output).slice(0);
-    // console.log(outputArray);
-
-    // outputBuffer.destroy();
-
-    // console.log("A", formatAsMatrix(matrixA, dimM, dimN));
-    // console.log("B", formatAsMatrix(matrixB, 1, dimN));
-    // console.log("Output:", formatAsMatrix(new Float32Array(output), dimM, dimN));
-    // const validation = new Float32Array(dimM * dimN);
-    // for (let i = 0; i < dimM; i++) for (let j = 0; j < dimN; j++) validation[i * dimN + j] = matrixA[i * dimN + j] + matrixB[j];
-    // console.log("Validation:", formatAsMatrix(validation, dimM, dimN));
-    // for (let i = 0; i < dimM * dimN; i++) if (output[i] !== validation[i]) return console.error("Validation failed");
+    // Median of times
+    times.sort((a, b) => a - b);
+    console.log("Median:", times[Math.floor(times.length / 2)]);
   }
 
   inlineFastRowAdd(commandEncoder, inputBuffer, biasBuffer, rows, cols) {
@@ -174,13 +155,13 @@ class TestGPT {
     const uniformBuffer = this.initBuffer(["uniform", "copy_to"], 4);
     const resultBuffer = this.initBuffer(["storage", "copy_from"], rows, cols);
     const bindGroup = this.initBindGroup(this.u_s_Layout, [uniformBuffer, resultBuffer]);
-    this.device.queue.writeBuffer(uniformBuffer, 0, new Uint32Array([rows, cols, cols / 4]));
+    this.device.queue.writeBuffer(uniformBuffer, 0, new Uint32Array([rows, cols]));
 
     const passEncoder = commandEncoder.beginComputePass();
     passEncoder.setPipeline(this.slowRowAddPipeline);
     passEncoder.setBindGroup(0, bindGroup);
     passEncoder.setBindGroup(1, this.initBindGroup(this.r_r_Layout, [inputBuffer, biasBuffer]));
-    passEncoder.dispatchWorkgroups(wgSize(rows, 8), wgSize(cols, 32));
+    passEncoder.dispatchWorkgroups(wgSize(rows, 8), wgSize(cols, 8));
     passEncoder.end();
 
     return resultBuffer;
@@ -207,11 +188,8 @@ class TestGPT {
       size: this.bufferSize(row, col),
       usage: ops.map((u) => bufferUsageDict[u]).reduce((a, b) => a | b),
     });
-    if (!noDelete) {
-      this.bufferDeletionStack.push(buffer);
-    } else {
-      this.unloadDeletionStack.push(buffer);
-    }
+    if (!noDelete) this.bufferDeletionStack.push(buffer);
+    else this.unloadDeletionStack.push(buffer);
     return buffer;
   }
 
@@ -223,6 +201,16 @@ class TestGPT {
 
   bufferSize(dimA, dimB = 1) {
     return Math.ceil((dimA * dimB * Float32Array.BYTES_PER_ELEMENT) / this.minBufferOffset) * this.minBufferOffset;
+  }
+
+  unloadBuffers() {
+    this.unloadDeletionStack.map((buffer) => buffer.destroy());
+    this.unloadDeletionStack = [];
+  }
+
+  destroyBuffers() {
+    this.bufferDeletionStack.map((buffer) => buffer.destroy());
+    this.bufferDeletionStack = [];
   }
 
   initBindGroups() {
@@ -242,9 +230,9 @@ class TestGPT {
     this.u_s_s_s_Layout = bg(["uniform", "storage", "storage", "storage"]);
   }
 
-  initPipelines() {
+  async initPipelines() {
     const p = (code, bindGroupLayouts) => {
-      return this.device.createComputePipeline({
+      return this.device.createComputePipelineAsync({
         layout: this.device.createPipelineLayout({ bindGroupLayouts }),
         compute: {
           module: this.device.createShaderModule({ code }),
@@ -253,8 +241,8 @@ class TestGPT {
       });
     };
 
-    this.fastRowAddPipeline = p(fastRowAddShader, [this.u_s_Layout, this.r_r_Layout]);
-    this.slowRowAddPipeline = p(slowRowAddShader, [this.u_s_Layout, this.r_r_Layout]);
+    this.fastRowAddPipeline = await p(fastRowAddShader, [this.u_s_Layout, this.r_r_Layout]);
+    this.slowRowAddPipeline = await p(slowRowAddShader, [this.u_s_Layout, this.r_r_Layout]);
   }
 }
 
